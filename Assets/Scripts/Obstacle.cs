1eb62008964d4d7ad5d8f0e52560b29be11e3ddc
Mariam Naziripour <MariamJ@lawn-128-61-67-3.lawn.gatech.edu>
2017-01-22 10:27:49 -0500

    Fixed multiple spawning issues

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+	public int obstacleHealth;
+	public string chatMessage;
+	public AudioSource obstacleSound;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/SpawnObstacle.cs
+++ Assets/Scripts/SpawnObstacle.cs
@@ -11,45 +11,60 @@ public class SpawnObstacle : MonoBehaviour {
 	private int waitTime;
 	public Transform wave;
 	public AudioSource upSound;
+	[SerializeField]
+	private float killDelay;
+	private float timer = 0;
+	public GameObject twitchClient;
 
 	void Start ()
 	{
 		alive = false;
+		StartCoroutine ("MakeObstacles");
 	}
 
 	void Update ()
 	{
-		if(alive == false){
-			StartCoroutine ("MakeObstacles");
+		if (alive == false) {
+//			timer += Time.deltaTime;
+//			if (timer >= killDelay) {
+				
+//				timer = 0;
+//			}
 		}
-	}
-
-	IEnumerator MakeObstacles(){
-		alive = true;
-		newTime (); 
-		yield return new WaitForSeconds (waitTime);
-		currentObstacle = obstacleSprites [Random.Range(0, 4)];
-		currentObstacle = Instantiate(currentObstacle,new Vector3(Random.Range(-5,7), -3 , 0),transform.rotation) as GameObject; 
-		MoveWaves.TransformWaves(wave, -.06f,upSound);
-	}
-
-	public void increaseCount(){
-		count += 1;
+		print ("count: " + count);
 		if(count==3){
 			alive = false;
 			killCreature ();
 		}
 	}
 
+	IEnumerator MakeObstacles(){
+		newTime (); 
+		yield return new WaitForSeconds (waitTime);
+		currentObstacle = obstacleSprites [Random.Range(0, 4)];
+		currentObstacle = Instantiate(currentObstacle,new Vector3(Random.Range(-5,7), -3 , 0),transform.rotation) as GameObject; 
+		MoveWaves.TransformWaves(wave, -.06f,upSound);
+		alive = true;
+		print ("obstacle made");
+	}
+
+	public void increaseCount(){
+		if (alive == true){
+			count += 1;
+			print ("increasing count");
+		}
+	}
+
 	public void killCreature(){
 			count = 0;
 			Destroy (currentObstacle);
-
+			twitchClient.GetComponent<TwitchIRC>().SendMsg ("Shoe has left. But she will be back");
+			StartCoroutine ("MakeObstacles");
 	}
 
 	public void newTime(){
 		waitTime = Random.Range (2, 10);
-		print ("Wait Time" + waitTime);
+	//	print ("Wait Time" + waitTime);
 
 	}
 		
