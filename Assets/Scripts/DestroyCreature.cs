894cd08cc1f8dca5573f5a27aef689a1e428b40d
Mariam Naziripour <MariamJ@lawn-128-61-67-3.lawn.gatech.edu>
2017-01-21 19:39:19 -0500

    Editing Destroy script

---
+++ Assets/Scripts/DestroyCreature.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyCreature : MonoBehaviour {
+	private GameObject currentObstacle;
+
+
+	// Use this for initialization
+	void Start () {
+		//currentObstacle = GameObject.FindGameObjectWithTag ("Obstacle");
+		//print (currentObstacle);
+	//	killCreature();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentObstacle = GameObject.FindGameObjectWithTag ("Obstacle");
+	}
+
+	void killCreature(){
+		if (currentObstacle != null) {
+			print ("There's something here!");
+		} else {
+			print ("nada");
+		}
+
+	}
+}
