f9b30f1f2d0a7f3916b40426c257c21cbb564c36
dsinger7 <dansinger644@gmail.com>
2017-01-22 11:57:38 -0500

    added fail state

--- Assets/Scripts/WaveControl.cs
+++ Assets/Scripts/WaveControl.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class WaveControl : MonoBehaviour {
 	private int upCount = 0;
 	private int downCount = 0;
-	private float waveHeight;
+	private float waveHeight = 5;
+	//sound effects from freesfx.com
 	public AudioSource upSound;
 	public AudioSource downSound;
 
+	private bool gameOver = false;
+
 	// Use this for initialization
 	void Start () {
-
+		
 	}
 	
 	// Update is called once per frame
@@ -22,12 +25,12 @@ public class WaveControl : MonoBehaviour {
 
 	public void commandDown(){
 		downCount += 1;
-		print(upCount);
+		//print(downCount);
 	}
 
 	public void commandUp(){
 		upCount += 1;
-		print (downCount);
+		//print (upCount);
 
 	}
 		
@@ -37,13 +40,28 @@ public class WaveControl : MonoBehaviour {
 			MoveWaves.TransformWaves(this.transform, -0.1f,upSound);
 			upCount = 0;
 			downCount = 0;
+			waveHeight = this.transform.position.y;
 		}
 		if (downCount >= 5) {
 			MoveWaves.TransformWaves(this.transform, 0.1f,downSound);
 			upCount = 0;
 			downCount = 0;
+			waveHeight = this.transform.position.y;
+		}
+
+		if(waveHeight > -1.5 || waveHeight < -11){
+			//print (waveHeight);
+			//gameOver = true;
+		}
+		if (gameOver) {
+			failState ();
 		}
 
 	}
 
+	public void failState(){
+		print ("Game Over");
+	}
+
 }
+
